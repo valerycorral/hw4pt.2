5e0631a48078041e957f4059c010fd429d2ff83c
valerycorral <vcorral@wesleyan.edu>
2022-04-27 13:45:06 -0400

    added rate-app

---
+++ rate-app/components/Rate.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View, ScrollView,Alert } from 'react-native';
+import {Button} from 'react-native-paper'
+
+
+export default function Rate(){
+
+  return(
+
+    <View style = {styles.container}></View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1E3D3',
+
+  },
+});
